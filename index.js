3239d931de87fe23f98e4ca75d55016e64cf7b3d
pranay <pranaypj10@gmail.com>
2020-06-21 13:56:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express')
+const pg = require('pg')
+const cors = require('cors');
+require('dotenv').config()
+
+
+const app = express()
+
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
+
+app.use(
+    cors({
+        origin: function(origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    })
+);
+
+
+const pool = new pg.Pool({
+  host: process.env.dbhost,
+  port: process.env.dbport,
+  database: process.env.database,
+  user: process.env.dbuser,
+  password: process.env.dbpassword
+})
+
+const queryHandler = (req, res, next) => {
+  pool.query(req.sqlQuery).then((r) => {
+    return res.json(r.rows || [])
+  }).catch(next)
+}
+
+app.get('/', (req, res) => {
+  
+  res.send('Welcome to EQ Works 😎')
+})
+
+app.get('/events/hourly', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, hour, events
+    FROM public.hourly_events
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/events/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, SUM(events) AS events
+    FROM public.hourly_events
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/stats/hourly', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date, hour, impressions, clicks, revenue
+    FROM public.hourly_stats
+    ORDER BY date, hour
+    LIMIT 168;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/stats/daily', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT date,
+        SUM(impressions) AS impressions,
+        SUM(clicks) AS clicks,
+        SUM(revenue) AS revenue
+    FROM public.hourly_stats
+    GROUP BY date
+    ORDER BY date
+    LIMIT 7;
+  `
+  return next()
+}, queryHandler)
+
+app.get('/poi', (req, res, next) => {
+  req.sqlQuery = `
+    SELECT *
+    FROM public.poi;
+  `
+  return next()
+}, queryHandler)
+
+app.listen(process.env.PORT || 5555, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  } else {
+    console.log(`Running on ${process.env.PORT || 5555}`)
+  }
+})
+
+
+
+
+// last resorts
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`)
+  process.exit(1)
+})
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
+  process.exit(1)
+})
